7869ab4fa5f8d3be03ea0595a5a2098689307e14
MinexAutomation <minex.automation@gmail.com>
2020-01-22 16:00:31 -0800

    Added ICDriveNameConvention service definition interface type.

--- source/R5T.Canterbury.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Canterbury.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Canterbury.Base/Code/Services/Definitions/ICDriveNameConvention.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Canterbury
+{
+    public interface ICDriveNameConvention
+    {
+        string GetCDriveName();
+    }
+}
